0e0e33c6274e902a2ad12c054555c37aed01a47d
ThiagoZx <TheThiagoo0@gmail.com>
2016-03-15 19:31:39 -0300

    Fill, Sort and Print
    It works, it's fine.

---
+++ SortingAlgorithm/SortingAlgorithm/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortingAlgorithm
+{
+    public partial class Form1 : Form
+    {
+
+        static int size = 10;
+        Random r = new Random ();
+        int[] array;
+
+        public Form1 () {
+            InitializeComponent ();
+        }
+
+        void fillArray (int _size) {
+            array = new int[size];
+            for (int i = 0; i < _size; i++) {
+                array[i] = r.Next (size);
+            }
+        }
+
+        void printArray () {
+            for (int i = 0; i < size; i++) {
+                Console.WriteLine (array[i]);
+            }
+        }
+
+        void bubbleSort (int[] _array) {
+            for (int i = 0; i < _array.Length; i++) {
+                for (int j = i + 1; j < _array.Length; j++) {
+                    if (_array[i] > _array[j]) {
+                        int temp = _array[i];
+                        _array[i] = _array[j];
+                        _array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click (object sender, EventArgs e) {
+            fillArray (size);
+            bubbleSort (array);
+            printArray ();
+        }
+    }
+}
