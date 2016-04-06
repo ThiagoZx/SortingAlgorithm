ed98d860d00d0f80f719ef3999700f5153e6276d
ThiagoZx <TheThiagoo0@gmail.com>
2016-04-05 23:50:10 -0300

    SearchMethods Class set
    Recursive times are coming.

---
+++ SortingAlgorithm/SortingAlgorithm/SeachMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm
+{
+    class SeachMethods
+    {
+        public int linearSeach (int element, int[] _array) {
+            return 1;
+        }
+
+        public int binarySearch (int element, int[] _array) {
+            return 1;
+        }
+    }
+}
